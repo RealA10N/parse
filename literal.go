ae644be2c58fad9be483d66b58e9da3e03b50e09
Alon Krymgand <me@alon.kr>
2024-08-09 20:16:15 +0300

    Added Literal Parser

---
+++ literal.go
@@ -0,0 +1,49 @@
+package parse
+
+import (
+	"fmt"
+	"iter"
+
+	"alon.kr/x/view"
+	"golang.org/x/exp/constraints"
+)
+
+type LiteralPropagator[TokenT any, NodeT any] func(TokenT) NodeT
+
+type literalParser[
+	TokenT comparable,
+	NodeT any,
+	OffsetT constraints.Unsigned,
+] struct {
+	propagator LiteralPropagator[TokenT, NodeT]
+	literal    TokenT
+}
+
+func Literal[
+	TokenT comparable,
+	NodeT any,
+	OffsetT constraints.Unsigned,
+](
+	propagator LiteralPropagator[TokenT, NodeT],
+	token TokenT,
+) NodeParser[TokenT, NodeT, OffsetT] {
+	return literalParser[TokenT, NodeT, OffsetT]{
+		propagator: propagator,
+		literal:    token,
+	}
+}
+
+func (p literalParser[TokenT, NodeT, OffsetT]) String() string {
+	return fmt.Sprintf("%v", p.literal)
+}
+
+func (p literalParser[TokenT, NodeT, OffsetT]) Parse(
+	v *view.View[TokenT, OffsetT],
+) iter.Seq[NodeT] {
+	return func(yield func(NodeT) bool) {
+		token, err := v.At(0)
+		if err == nil {
+			yield(p.propagator(token))
+		}
+	}
+}
